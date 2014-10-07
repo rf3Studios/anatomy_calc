f6ee1470434da1960190497e576feeef23bc409e
Rich Friedel <rf3studios.apps@gmail.com>
2014-10-06 21:42:05 -0400

    Bugfix, streamline code and code formatting Fixed bug where the user could manually enter a number larger than the max in a spinner input. Streamlined some of the selectors Formatted code

--- scripts/grade_utils.js
+++ scripts/grade_utils.js
@@ -24,11 +24,11 @@
 /**
  * Init all scripts on page ready...
  */
-$( document ).ready( function() {
+$(document).ready(function () {
     buildNumberSpinners();
     buildInfoTooltips();
-    showGrade( generateFinalGrade() );
-} );
+    showGrade(generateFinalGrade());
+});
 
 
 /**
@@ -38,41 +38,58 @@ $( document ).ready( function() {
  */
 function buildNumberSpinners() {
     // Exam Spinners
-    $( "input[name*='lec_exam_']" ).spinner( {min:0, max:100, step:0.01, numberFormat:"n", stop:function() {
-        showGrade( generateFinalGrade() );
-    }} ).val( 100 );
-    $( "input[name*='lab_exam_']" ).spinner( {min:0, max:100, step:0.01, numberFormat:"n", stop:function() {
-        showGrade( generateFinalGrade() );
-    }} ).val( 100 );
-
-    // Final Exam Spinners
-    $( "#lec_final_exam" ).spinner( {min:0, max:100, step:0.01, numberFormat:"n", stop:function() {
-        showGrade( generateFinalGrade() );
-    }} ).val( 100 );
-    $( "#lab_final_exam" ).spinner( {min:0, max:100, step:0.01, numberFormat:"n", stop:function() {
-        showGrade( generateFinalGrade() );
-    }} ).val( 100 );
+    $("input[name*='lec_exam_'], input[name*='lab_exam_'], #lec_final_exam, #lab_final_exam").spinner({
+        min: 0,
+        max: 100,
+        step: 0.01,
+        numberFormat: "n",
+        stop: function () {
+            $(this).spinner();
+            showGrade(generateFinalGrade());
+        }
+    }).val(100);
 
     // Quiz Spinners
-    $( "input[name*='lec_quiz']" ).spinner( {min:0, max:10, step:1, stop:function() {
-        showGrade( generateFinalGrade() );
-    }} ).val( 10 );
-    $( "#lec_quiz_7" ).spinner( {min:0, max:12, step:1, stop:function() {
-        showGrade( generateFinalGrade() );
-    }} ).val( 12 );
+    $("input[name*='lec_quiz']").spinner({
+        min: 0,
+        max: 10,
+        step: 1,
+        stop: function () {
+            $(this).spinner();
+            showGrade(generateFinalGrade());
+        }
+    }).val(10);
+    $("#lec_quiz_7").spinner({
+        min: 0,
+        max: 12,
+        step: 1,
+        stop: function () {
+            $(this).spinner();
+            showGrade(generateFinalGrade());
+        }
+    }).val(12);
 
     // Participation Spinner
-    $( "#lab_participation" ).spinner( {min:0, max:100, step:0.01, numberFormat:"n", stop:function() {
-        showGrade( generateFinalGrade() );
-    }} ).val( 100 );
+    $("#lab_participation").spinner({
+        min: 0,
+        max: 100,
+        step: 0.01,
+        numberFormat: "n",
+        stop: function () {
+            $(this).spinner();
+            showGrade(generateFinalGrade());
+        }
+    }).val(100);
 
     // Attendance Spinners
-    $( "#lec_attendance" ).spinner( {min:0, step:1, stop:function() {
-        showGrade( generateFinalGrade() );
-    }} ).val( 0 );
-    $( "#lab_attendance" ).spinner( {min:0, step:1, stop:function() {
-        showGrade( generateFinalGrade() );
-    }} ).val( 0 );
+    $("#lec_attendance, #lab_attendance").spinner({
+        min: 0,
+        step: 1,
+        stop: function () {
+            $(this).spinner();
+            showGrade(generateFinalGrade());
+        }
+    }).val(0);
 }
 
 /**
@@ -80,19 +97,46 @@ function buildNumberSpinners() {
  */
 function buildInfoTooltips() {
     // Lecture Legend Tooltips
-    $( "#lec_exams_wrapper" ).find( "legend img.info_icon" ).tooltip( {tooltipClass:"info_tooltip", content:"<p><strong>Lecture Exams:</strong> Enter exam grade percentages as numbers (e.g. 85% should be entered as 85)</p><p>Exam grade is calculated by taking the sum of all exams and dividing that total by the amount of exams.</p><p><strong>NOTE:</strong> If Exam 2 grade is larger than the Exam 1 grade, then the calculation will take the difference of Exam 2 - Exam 1, divide that by 2 and add that to the Exam 1 grade.</p><p>e.g. Exam 1 = ((Exam 2 - Exam 1) / 2) + Exam 1</p>"} );
-    $( "#lec_final_exam_wrapper" ).find( "legend img.info_icon" ).tooltip( {tooltipClass:"info_tooltip", content:"<p><strong>Lecture Final Exams:</strong> Enter final exam grade percentage as a number (e.g. 85% should be entered as 85)</p>"} );
-    $( "#lec_quizzes_wrapper" ).find( "legend img.info_icon" ).tooltip( {tooltipClass:"info_tooltip", content:"<p><strong>Lecture Quizzes:</strong> Enter quiz grades. Quiz grade average is calculated as follows...</p><p>Quiz Avg = Sum of all quizzes / Total possible points</p><p><strong>NOTE:</strong> The lowest grade is not used while calculating the final quiz grade average.</p>"} );
-    $( "#lec_attendance_wrapper" ).find( "legend img.info_icon" ).tooltip( {tooltipClass:"info_tooltip", content:"<p><strong>Lecture Attendance:</strong> Enter attendance as number of days missed.</p><p><strong>NOTE:</strong> <br>0-3 Days Missed: 0pts<br>4-7 Days Missed: 2pts/day<br>7+ Days Missed: &nbsp;Automatic Failure</p>"} );
+    $("#lec_exams_wrapper").find("legend img.info_icon").tooltip({
+        tooltipClass: "info_tooltip",
+        content: "<p><strong>Lecture Exams:</strong> Enter exam grade percentages as numbers (e.g. 85% should be entered as 85)</p><p>Exam grade is calculated by taking the sum of all exams and dividing that total by the amount of exams.</p><p><strong>NOTE:</strong> If Exam 2 grade is larger than the Exam 1 grade, then the calculation will take the difference of Exam 2 - Exam 1, divide that by 2 and add that to the Exam 1 grade.</p><p>e.g. Exam 1 = ((Exam 2 - Exam 1) / 2) + Exam 1</p>"
+    });
+    $("#lec_final_exam_wrapper").find("legend img.info_icon").tooltip({
+        tooltipClass: "info_tooltip",
+        content: "<p><strong>Lecture Final Exams:</strong> Enter final exam grade percentage as a number (e.g. 85% should be entered as 85)</p>"
+    });
+    $("#lec_quizzes_wrapper").find("legend img.info_icon").tooltip({
+        tooltipClass: "info_tooltip",
+        content: "<p><strong>Lecture Quizzes:</strong> Enter quiz grades. Quiz grade average is calculated as follows...</p><p>Quiz Avg = Sum of all quizzes / Total possible points</p><p><strong>NOTE:</strong> The lowest grade is not used while calculating the final quiz grade average.</p>"
+    });
+    $("#lec_attendance_wrapper").find("legend img.info_icon").tooltip({
+        tooltipClass: "info_tooltip",
+        content: "<p><strong>Lecture Attendance:</strong> Enter attendance as number of days missed.</p><p><strong>NOTE:</strong> <br>0-3 Days Missed: 0pts<br>4-7 Days Missed: 2pts/day<br>7+ Days Missed: &nbsp;Automatic Failure</p>"
+    });
 
     // Lab Legend Tooltips
-    $( "#lab_exams_wrapper" ).find( "legend img.info_icon" ).tooltip( {tooltipClass:"info_tooltip", content:"<p><strong>Lab Practicals:</strong> Enter practical grade percentages as numbers (e.g. 85% should be entered as 85)</p><p><strong>NOTE:</strong> Each exam is worth 20% of the total. <br><br>e.g. Total = (Practical 1 + Practical 2 + Practical 3) / 3</p>"} );
-    $( "#lab_final_exam_wrapper" ).find( "legend img.info_icon" ).tooltip( {tooltipClass:"info_tooltip", content:"<p><strong>Lab Final Exams:</strong> Enter final exam grade percentage as a number (e.g. 85% should be entered as 85)</p>"} );
-    $( "#lab_participation_wrapper" ).find( "legend img.info_icon" ).tooltip( {tooltipClass:"info_tooltip", content:"<p><strong>Lab Participation:</strong> Enter participation grade percentage as a number (e.g. 85% should be entered as 85)</p>"} );
-    $( "#lab_attendance_wrapper" ).find( "legend img.info_icon" ).tooltip( {tooltipClass:"info_tooltip", content:"<p><strong>Lab Attendance:</strong>> Enter attendance as number of days missed.</p><p><strong>NOTE:</strong> For each day missed, the final lab grade is reduced by 2%</p><p>e.g. 2 days missed results in a 4% reduction</p>"} );
+    $("#lab_exams_wrapper").find("legend img.info_icon").tooltip({
+        tooltipClass: "info_tooltip",
+        content: "<p><strong>Lab Practicals:</strong> Enter practical grade percentages as numbers (e.g. 85% should be entered as 85)</p><p><strong>NOTE:</strong> Each exam is worth 20% of the total. <br><br>e.g. Total = (Practical 1 + Practical 2 + Practical 3) / 3</p>"
+    });
+    $("#lab_final_exam_wrapper").find("legend img.info_icon").tooltip({
+        tooltipClass: "info_tooltip",
+        content: "<p><strong>Lab Final Exams:</strong> Enter final exam grade percentage as a number (e.g. 85% should be entered as 85)</p>"
+    });
+    $("#lab_participation_wrapper").find("legend img.info_icon").tooltip({
+        tooltipClass: "info_tooltip",
+        content: "<p><strong>Lab Participation:</strong> Enter participation grade percentage as a number (e.g. 85% should be entered as 85)</p>"
+    });
+    $("#lab_attendance_wrapper").find("legend img.info_icon").tooltip({
+        tooltipClass: "info_tooltip",
+        content: "<p><strong>Lab Attendance:</strong>> Enter attendance as number of days missed.</p><p><strong>NOTE:</strong> For each day missed, the final lab grade is reduced by 2%</p><p>e.g. 2 days missed results in a 4% reduction</p>"
+    });
 
     // Grade Information
-    $( "#info_output" ).find( "h3 img.info_icon" ).tooltip( {tooltipClass:"info_tooltip info_tooltip_grade_final", content:"<p><strong>Grade Information:</strong> The final grade total is calculated as follows...</p><p><strong>Lecture:</strong><br>((((Exam1 + Exam2 + Exam3 + Exam4) / 4) * 0.7) + (((Quiz Total / Total Possible) * 100) * 0.1) + (FinalExam * 0.2)) * 0.75</p><p><strong>+</strong></p><p><strong>Lab:</strong><br>(((((Practical1 + Practical2 + Practical3) / 3) * 0.6) + (Final Practical * 0.3) + (Participation * 0.1)) * 0.25) * (Days Missed * 0.02)</p>"} );
+    $("#info_output").find("h3 img.info_icon").tooltip({
+        tooltipClass: "info_tooltip info_tooltip_grade_final",
+        content: "<p><strong>Grade Information:</strong> The final grade total is calculated as follows...</p><p><strong>Lecture:</strong><br>((((Exam1 + Exam2 + Exam3 + Exam4) / 4) * 0.7) + (((Quiz Total / Total Possible) * 100) * 0.1) + (FinalExam * 0.2)) * 0.75</p><p><strong>+</strong></p><p><strong>Lab:</strong><br>(((((Practical1 + Practical2 + Practical3) / 3) * 0.6) + (Final Practical * 0.3) + (Participation * 0.1)) * 0.25) * (Days Missed * 0.02)</p>"
+    });
 }
 
 /**
@@ -100,12 +144,12 @@ function buildInfoTooltips() {
  *
  * @param finalGrade The final grade in number format
  */
-function showGrade( finalGrade ) {
-    $( "#total_num_grade" ).html( finalGrade );
-    $( "#total_letter_grade" ).html( convertToLetterGrade( finalGrade ) );
+function showGrade(finalGrade) {
+    $("#total_num_grade").html(finalGrade);
+    $("#total_letter_grade").html(convertToLetterGrade(finalGrade));
 
     // Highlight the grade scale
-    highlightGradeScale( convertToLetterGrade( finalGrade ) );
+    highlightGradeScale(convertToLetterGrade(finalGrade));
 }
 
 /**
@@ -113,16 +157,16 @@ function showGrade( finalGrade ) {
  *
  * @param letterGrade
  */
-function highlightGradeScale( letterGrade ) {
+function highlightGradeScale(letterGrade) {
     // Reset the styles
-    $( "dd" ).css( {"background-color":"#FFF", "font-weight":"normal"} );
+    $("dd").css({"background-color": "#FFF", "font-weight": "normal"});
 
     var _match = "";
     var _color = "";
 
     // TODO Can this be improved?
     // Set the new style
-    switch( letterGrade ) {
+    switch (letterGrade) {
         case "A":
             _match = "89.50 - 100.0 = A";
             _color = "#12D600"; // Bright Green
@@ -157,9 +201,9 @@ function highlightGradeScale( letterGrade ) {
             break;
     }
 
-    $( "dd" ).filter(function() {
-        return $( this ).text() === _match;
-    } ).css( {"background-color":_color, "font-weight":"bold"} );
+    $("dd").filter(function () {
+        return $(this).text() === _match;
+    }).css({"background-color": _color, "font-weight": "bold"});
 }
 
 /**
@@ -169,12 +213,12 @@ function highlightGradeScale( letterGrade ) {
  */
 function getLectureExamGrades() {
     // Get the lecture exam values
-    var _lecExamsArr = $( "input[name*='lec_exam']" );
+    var _lecExamsArr = $("input[name*='lec_exam']");
     var _lecExamVals = [];
 
-    $.each( _lecExamsArr, function( i, items ) {
-        _lecExamVals[i] = parseFloat( $( items ).val() );
-    } );
+    $.each(_lecExamsArr, function (i, items) {
+        _lecExamVals[i] = parseFloat($(items).val());
+    });
 
     return _lecExamVals;
 }
@@ -186,12 +230,12 @@ function getLectureExamGrades() {
  */
 function getLabExamGrades() {
     // Get lab exams
-    var _labExamsArr = $( "input[name*='lab_exam']" );
+    var _labExamsArr = $("input[name*='lab_exam']");
     var _labExamVals = [];
 
-    $.each( _labExamsArr, function( k, items ) {
-        _labExamVals[k] = parseFloat( $( items ).val() );
-    } );
+    $.each(_labExamsArr, function (k, items) {
+        _labExamVals[k] = parseFloat($(items).val());
+    });
 
     return _labExamVals;
 }
@@ -204,12 +248,12 @@ function getLabExamGrades() {
  */
 function getLectureQuizGrades() {
     // Get the lecture quiz values
-    var _lecQuizArr = $( "input[name*='lec_quiz']" );
+    var _lecQuizArr = $("input[name*='lec_quiz']");
     var _lecQuizVals = [];
 
-    $.each( _lecQuizArr, function( j, items ) {
-        _lecQuizVals[j] = parseFloat( $( items ).val() );
-    } );
+    $.each(_lecQuizArr, function (j, items) {
+        _lecQuizVals[j] = parseFloat($(items).val());
+    });
 
     // TODO Create a way so that the base values can be manually entered from the form
     // Quiz base values
@@ -238,32 +282,32 @@ function generateFinalGrade() {
     var _lab = new Lab();
 
     // Get the lecture final exam value
-    var _lecFinalExamVal = parseFloat( $( "input[name='lec_final_exam']" ).val() );
+    var _lecFinalExamVal = parseFloat($("input[name='lec_final_exam']").val());
 
     // Get the lecture attendance
-    var _lecAttendanceVal = parseInt( $( "input[name='lec_attendance']" ).val() );
+    var _lecAttendanceVal = parseInt($("input[name='lec_attendance']").val());
 
     /* LAB */
     // Get lab final exam
-    var _labFinalExamVal = parseFloat( $( "input[name='lab_final_exam']" ).val() );
+    var _labFinalExamVal = parseFloat($("input[name='lab_final_exam']").val());
 
     // Get lab participation
-    var _labParticipationVal = parseFloat( $( "input[name='lab_participation']" ).val() );
+    var _labParticipationVal = parseFloat($("input[name='lab_participation']").val());
 
     // Get lab attendance
-    var _labAttendanceVal = parseFloat( $( "input[name='lab_attendance']" ).val() );
+    var _labAttendanceVal = parseFloat($("input[name='lab_attendance']").val());
 
-    var _lecTotal = _lec.calculateWeightedTotal( getLectureExamGrades(), getLectureQuizGrades(), _lecFinalExamVal,
-                                                 _lecAttendanceVal );
+    var _lecTotal = _lec.calculateWeightedTotal(getLectureExamGrades(), getLectureQuizGrades(), _lecFinalExamVal,
+        _lecAttendanceVal);
 
-    var _labTotal = _lab.calculateWeightedTotal( getLabExamGrades(), _labFinalExamVal, _labParticipationVal,
-                                                 _labAttendanceVal );
+    var _labTotal = _lab.calculateWeightedTotal(getLabExamGrades(), _labFinalExamVal, _labParticipationVal,
+        _labAttendanceVal);
 
-    if( _lecTotal === -1 ) {
+    if (_lecTotal === -1) {
         return 0;
     }
     else {
-        return (_lecTotal + _labTotal).toFixed( 8 );
+        return (_lecTotal + _labTotal).toFixed(8);
     }
 }
 
@@ -283,33 +327,33 @@ function generateFinalGrade() {
  * @param {number} numGrade Numerical grade percentage
  * @returns {string} Letter equivalent of a numerical grade
  */
-function convertToLetterGrade( numGrade ) {
+function convertToLetterGrade(numGrade) {
 
-    if( numGrade >= 89.5 ) {
+    if (numGrade >= 89.5) {
         return "A";
     }
     else {
-        if( numGrade >= 88.5 && numGrade < 89.5 ) {
+        if (numGrade >= 88.5 && numGrade < 89.5) {
             return "B+";
         }
         else {
-            if( numGrade >= 79.5 && numGrade < 88.5 ) {
+            if (numGrade >= 79.5 && numGrade < 88.5) {
                 return "B";
             }
             else {
-                if( numGrade >= 78.5 && numGrade < 79.5 ) {
+                if (numGrade >= 78.5 && numGrade < 79.5) {
                     return "C+";
                 }
                 else {
-                    if( numGrade >= 69.5 && numGrade < 78.5 ) {
+                    if (numGrade >= 69.5 && numGrade < 78.5) {
                         return "C";
                     }
                     else {
-                        if( numGrade >= 68.5 && numGrade < 69.5 ) {
+                        if (numGrade >= 68.5 && numGrade < 69.5) {
                             return "D+";
                         }
                         else {
-                            if( numGrade >= 59.5 && numGrade < 68.5 ) {
+                            if (numGrade >= 59.5 && numGrade < 68.5) {
                                 return "D";
                             }
                             else {
